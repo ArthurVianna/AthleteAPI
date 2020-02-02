6ee74ce1208a9b83abdb96bc8a2883bf9aa1e1d6
Arthur Vianna <arthurvlandeo@gmail.com>
2020-02-02 16:10:57 -0300

    Add csv import method following the kaggle csv model

---
+++ Athlete/importAthleteCSV.py
@@ -0,0 +1,72 @@
+import csv
+from Athlete.models import *
+
+def readFileAsDict(path):
+    return csv.DictReader(open(path))
+
+
+def importCSV(path):
+    openedCSV = readFileAsDict(path)
+    
+    for row in openedCSV:
+        
+        athlete = athleteImport(row)
+        olympic = olympicsImport(row)
+        event = eventImport(row)
+        city,created =  City.objects.get_or_create(name=row["City"])
+
+        """for key,value in row.items():
+            if key in columnKeys.keys():
+                kwargs[columnKeys[key]] = value"""
+        kwargs = {}
+        kwargs["athlete"] = athlete
+        kwargs["olympic"] = olympic
+        kwargs["city"] = city
+        kwargs["event"] = event
+        kwargs["age"] = parseToIntOrNone(row["Age"])
+        if(row["Medal"] != "NA"):
+            kwargs["medal"] = row["Medal"]
+        kwargs["team"] = row["Team"]
+
+
+        Participation.objects.get_or_create(**kwargs)
+    
+        
+
+def athleteImport(rowDict):
+    args = {}
+    args["name"] = rowDict["Name"]
+    args["sex"] = rowDict["Sex"]
+    args["height"] = parseToIntOrNone(rowDict["Height"])
+    args["weight"] = parseToIntOrNone(rowDict["Weight"])
+    args["noc"],created = NOC.objects.get_or_create(noc=rowDict["NOC"])
+
+    athlete,created = Athlete.objects.get_or_create(**args)
+    return athlete
+
+def olympicsImport(rowDict):
+    args = {}
+    
+    args["name"] = rowDict["Games"]
+    args["year"] = parseToIntOrNone(rowDict["Year"])
+    args["Season"] = rowDict["Season"]
+
+    olymp,created = Olympics.objects.get_or_create(**args)
+    return olymp
+
+def eventImport(rowDict):
+    args = {}
+    
+    args["sport"],created = Sport.objects.get_or_create(name=rowDict["Sport"])
+    args["name"] = rowDict["Event"]
+
+    event,created = Event.objects.get_or_create(**args)
+    return event
+
+def parseToIntOrNone(str):
+    try:
+        return int(str)
+    except ValueError:
+        return None
+
+
